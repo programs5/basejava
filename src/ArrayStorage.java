721849268dad8f0f610f5581ec8560170fec8f23
Malygin <Pavel.Malygin@billing.ru>
2019-09-11 12:37:23 +0300

    11092019_1237

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,35 +7,38 @@ import java.util.NoSuchElementException;
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
+    // полностью очищаем хранилище
     void clear() {
-
-    }
-
-    // save Resume
-    void save(Resume r) {
-        boolean isFull = true;
-
         for(int i= 0; i < storage.length; i++){
-            if(storage[i] == null) {
-                storage[i] = r;
-                isFull = false;
+            if(storage[i] == null)
                 break;
-            }
+            else
+                storage[i] = null;
         }
-        if(isFull) throw new ArrayIndexOutOfBoundsException();
+        size = 0;
     }
 
-    // get Resume
+    // добавляем новый элемент в хранилище
+    void save(Resume r) {
+        // если хранилище заполнено, гененрируем ошибку
+        if(size == 10000)
+            throw new ArrayIndexOutOfBoundsException();
+        // добавляем элемент
+        storage[size] = r;
+        size++;
+    }
+
+    // возвращаем элемент по заданному uuid
     Resume get(String uuid) {
-
         int idx = -1;
-
         for(int i= 0; i < storage.length; i++){
-
-            if(storage[i] == null) throw new NoSuchElementException(uuid);
-
-            if(storage[i].uuid == uuid) {
+            // если перебрали все элементы и не нашли нужного, генерируем ошибку
+            if(storage[i] == null) {
+                throw new NoSuchElementException(uuid);
+            }
+            else if(storage[i].uuid == uuid) {
                 idx= i;
                 break;
             }
@@ -43,21 +46,41 @@ public class ArrayStorage {
         return storage[idx];
     }
 
+    // удаляем элемент с заданным uuid из очереди
     void delete(String uuid) {
-
+        boolean isDeleted = false;
+        for(int i= 0; i < storage.length; i++){
+            // если перебрали все элементы и не нашли нужного, генерируем ошибку
+            if(!isDeleted && storage[i] == null) {
+                throw new NoSuchElementException(uuid);
+            }
+            // если удалили элемент и больше нет элементов, завершаем
+            if(isDeleted && storage[i] == null ){
+                break;
+            }
+            // если удалили и есть еще элементы, смещаем их на место удаленного, закрываем все дырки
+            if(isDeleted && storage[i] != null){
+                storage[i-1] = storage[i];
+                storage[i] = null;
+            }
+            // ищем элемент и если совпадает удаляем его
+            if(!isDeleted && storage[i] != null) {
+                if (storage[i].uuid == uuid) {
+                    storage[i] = null;
+                    isDeleted = true;
+                    size--;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        Resume[] resumes;
-        int count = size();
-        resumes = new Resume[count];
-
-        if(count > 0){
-            for(int i= 0; i < count; i++) {
+        Resume[] resumes = new Resume[size];
+        if(size > 0){
+            for(int i= 0; i < size; i++) {
                 resumes[i] = new Resume();
                 resumes[i].uuid = storage[i].uuid;
             }
@@ -65,17 +88,8 @@ public class ArrayStorage {
         return resumes;
     }
 
-    // size
+    // возвращем количество элементов хранилища
     int size() {
-        int size = 0;
-
-        for(int i= 0; i < storage.length; i++){
-
-            if(storage[i] == null) {
-                size = i;
-                break;
-            }
-        }
         return size;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -31,22 +31,14 @@ public class MainTestArrayStorage {
 
         //System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
-
-
         printAll();
-
-
-        /*
         ARRAY_STORAGE.delete(r1.uuid);
         printAll();
-
-
 
         ARRAY_STORAGE.clear();
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
-        */
     }
 
     static void printAll() {
