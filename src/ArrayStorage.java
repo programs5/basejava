15ab407c6b2c486bdf229b850784b138b5d1e261
programs5 <Malyginpoleg5>
2019-09-14 12:50:40 +0300

    First version homework (lesson#02)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,6 +7,16 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size = 0;
 
+    // FIND
+    int find(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     // CLEAR
     void clear() {
         for (int i = 0; i < size; i++) {
@@ -15,20 +25,10 @@ public class ArrayStorage {
         size = 0;
     }
 
-    // FIND
-    int find(String uid) {
-        for (int i = 0; i < size; i++) {
-            if(storage[i].uuid == uid) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     // UPDATE
     void update(Resume r) {
         int idx = find(r.uuid);
-        if(idx >= 0)
+        if (idx >= 0)
             storage[idx] = r;
         else
             System.out.println("UpdateError: Is No Resume Found");
@@ -40,36 +40,19 @@ public class ArrayStorage {
             System.out.println("Error: Array Index Out Of Bounds");
             return;
         }
-        int idx = find(r.uuid);
-        if(idx >= 0) {
+        if (find(r.uuid) >= 0) {
             System.out.println("SaveError: Resume Already Present");
             return;
         }
         storage[size] = r;
         size++;
-        /*
-        // TODO check if resume not present (out error message)
-        if (size == storage.length) {
-            System.out.println("Error: Array Index Out Of Bounds");
-            return;
-        }
-        for (int i = 0; i < size; i++) {
-            if (storage[i] == r) {
-                System.out.println("SaveError: Resume Already Present");
-                return;
-            }
-        }
-        storage[size] = r;
-        size++;
-        */
     }
 
     // GET
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                return storage[i];
-            }
+        int idx = find(uuid);
+        if (idx >= 0) {
+            return storage[idx];
         }
         return null;
     }
@@ -77,25 +60,13 @@ public class ArrayStorage {
     // DELETE
     void delete(String uuid) {
         int idx = find(uuid);
-        if(idx >= 0) {
+        if (idx >= 0) {
             storage[idx] = storage[size - 1];
             storage[size - 1] = null;
             size--;
             return;
         }
         System.out.println("DeleteError: Is No Resume Found");
-        /*
-        // TODO check if resume present (out error message)
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                return;
-            }
-        }
-        System.out.println("DeleteError: Is No Resume Found");
-        */
     }
 
     // GET ALL
