d3e74f9335b06042d94a278c4d68947023fb2804
programs5 <pavel.malygin@gmail.com>
2019-10-11 23:51:35 +0300

    HW3: 1. optimized methods saveSpecial and deleteSpecial (SortedArrayStorage)

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -8,17 +8,13 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void saveSpecial(Resume resume, int idx) {
-        if (size > 0) {
-            System.arraycopy(storage, idx - 1, storage, idx, size - idx + 1);
-        }
+        System.arraycopy(storage, idx - 1, storage, idx, size - idx + 1);
         storage[idx - 1] = resume;
     }
 
     @Override
     protected void deleteSpecial(String uuid, int idx) {
-        if (idx < size - 1) {
-            System.arraycopy(storage, idx + 1, storage, idx, size - idx - 1);
-        }
+        System.arraycopy(storage, idx + 1, storage, idx, size - idx - 1);
     }
 
     @Override
