fd769798980bff274bec58df5f1c76622d7cbd24
programs5 <pavel.malygin@gmail.com>
2019-10-21 22:55:34 +0300

    HW4: 1. Changed MainReflection 2. Created test AbstractArrayStorageTest

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -8,8 +8,12 @@ public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
         System.out.println(field.getName());
         field.get(resume);
+        System.out.println(resume);
+        field.set(resume, "555");
+        // TODO : invoke resume.toString via reflection
         System.out.println(resume);
     }
 }
---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
