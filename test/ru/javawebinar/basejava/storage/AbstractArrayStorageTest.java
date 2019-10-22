2b2911b15c6509c517c66de6a9b4864ff65cd395
programs5 <pavel.malygin@gmail.com>
2019-10-22 22:53:04 +0300

    HW4: 1. Created *StorageTest classes

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -1,36 +1,61 @@
 package ru.javawebinar.basejava.storage;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
 
 import static org.junit.Assert.*;
 
-public class AbstractArrayStorageTest {
+// TODO реализовать проверку на переполнение overflow тоже !!!
 
-    @Test
-    public void size() {
+public abstract class AbstractArrayStorageTest {
+
+    private Storage storage; // = new ArrayStorage();
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
     }
 
     @Test
-    public void clear() {
+    public void size() throws Exception {
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
-    public void update() {
+    public void clear() throws Exception {
     }
 
     @Test
-    public void save() {
+    public void update() throws Exception {
     }
 
     @Test
-    public void delete() {
+    public void save() throws Exception {
     }
 
     @Test
-    public void get() {
+    public void delete() throws Exception {
     }
 
     @Test
-    public void getAll() {
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
     }
 }
